522dc3fcf006a13a0866ab368c580037b9a0eddb
Mara Rosa <edimara.rosa@hotmail.com>
2019-11-26 17:51:40 -0300

    add função que ocnverte o json(string) em um objeto

--- requestTest.js
+++ requestTest.js
@@ -1,7 +1,9 @@
 var request = require('request');
-request('https://github.com/mararosa', function (error, response, body) {
+request('http://api.openweathermap.org/data/2.5/weather?q=SaoPaulo&APPID=YOUR_KEY', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
+  var parsedWeather = JSON.parse(body);
+  console.log('A temperatura atual em São Paulo é ' + parsedWeather['main']['temp']); // Print the Temperature in the city of São Paulo
 });
 
